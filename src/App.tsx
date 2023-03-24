df1691a7eebdc20e7eadc9dcf7e37a1ecaccd752
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2023-03-24 18:40:33 +0100

    Added basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import {Routes, Route, NavLink} from 'react-router-dom';
 
 export default function App () {
     return (
-        <h1>Hello world</h1>
+        <main>
+            <header className='main-header'>AoC 2022 visualizations</header>
+            <nav>
+                <NavLink to="/Day10" className="nav-item">Day10</NavLink>
+                <NavLink to="/Day17" className="nav-item">Day17</NavLink>
+            </nav>
+            <Routes>
+                <Route path="/" element={<p>This is main route</p>} />
+                <Route path="/Day10" element={<p>This is Day10</p>} />
+                <Route path="/Day17" element={<p>This is Day17</p>} />
+            </Routes>
+        </main>
     )
 }
