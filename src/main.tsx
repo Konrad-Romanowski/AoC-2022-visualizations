6daedd6c11877b95c91bdf113559febfbad6acbe
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2023-05-09 18:44:05 +0200

    Solved problem with refreshing page after changing Route

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/AoC-2022-visualizations">
             <App />
         </BrowserRouter>
     </React.StrictMode>,
